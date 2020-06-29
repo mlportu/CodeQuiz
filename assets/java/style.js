4a5ab2449c309558929d544739eb6d66f5b1d812
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-06-29 09:22:27 -0500

    restructure html and js

--- assets/java/style.js
+++ assets/java/style.js
@@ -4,7 +4,10 @@ var startButton = document.querySelector("#start-quiz");
 var questionContainerEl = document.querySelector("#question-form");
 var welcome = document.querySelector("#welcome");
 var currentQuesitonIndex = 0;
-
+var questionEl = document.querySelector("#question");
+var aAnswerEl = document.querySelector("#button-a")
+var bAnswerEl = document.querySelector("#button-b");
+var cAnswerEl = document.querySelector("#button-c");
 
 //timer
 var seconds=60;
@@ -43,18 +46,18 @@ var questions = [
     {
       question: "Question2",
       answers: {
-        a: "Answer 1",
-        b: "Answer 2",
-        c: "Answer 3"
+        a: "Answer 4",
+        b: "Answer 5",
+        c: "Answer 6"
       },
       correctAnswer: "c"
     },
     {
       question: "Question3",
       answers: {
-        a: "Answer 1",
-        b: "Answer 2",
-        c: "Answer 3"
+        a: "Answer 7",
+        b: "Answer 8",
+        c: "Answer 9"
       },
       correctAnswer: "c"
     }
@@ -63,52 +66,52 @@ var startQuiz = function (){
   startButton.classList.add("hide");
   welcome.classList.add("hide");
   questionContainerEl.classList.remove("hide");
+  aAnswerEl.classList.remove("hide");
 
   renderQuestion();
  };
 
 var renderQuestion = function (){
-      var q = questions[currentQuesitonIndex];
-      question.innerHTML = "<h2 id='question'>" + q.question + "</h2>";
+  var q = questions[currentQuesitonIndex];
       
-      var aAnswerEl = document.createElement("button");
-      aAnswerEl.className = "btn";
-      aAnswerEl.style = "margin: 15px";
-      aAnswerEl.innerHTML = "<h3 id=answer>" + q.answers.a +"</h3>";
+  if (currentQuesitonIndex < questions.length) {
+      questionEl.innerHTML = "<h2 id='question'>" + q.question + "</h2>";
+      
+      aAnswerEl.innerHTML = "<h3 id = 'button-a'>" + q.answers.a +"</h3>";
       aAnswerEl.setAttribute("value", "a")
-      questionContainerEl.appendChild(aAnswerEl);
-      aAnswerEl.addEventListener("click", selectAnswer);
+      console.log(q.answers.a)
 
-      var bAnswerEl = document.createElement("button");
-      bAnswerEl.className = "btn";
-      bAnswerEl.setAttribute("value", "b")
-      bAnswerEl.style = "margin: 15px";
       bAnswerEl.innerHTML = "<h3 id=answer>" + q.answers.b +"</h3>";
-      questionContainerEl.appendChild(bAnswerEl);
+      bAnswerEl.setAttribute("value", "b");
       bAnswerEl.addEventListener("click", selectAnswer);
 
-      var cAnswerEl = document.createElement("button");
-      cAnswerEl.className = "btn";
-      cAnswerEl.setAttribute("value", "c")
-      cAnswerEl.style = "margin: 15px";
       cAnswerEl.innerHTML = "<h3 id=answer>" + q.answers.c +"</h3>";
-      questionContainerEl.appendChild(cAnswerEl);
+      cAnswerEl.setAttribute("value", "c");
       cAnswerEl.addEventListener("click", selectAnswer);
     }
+  else{
+    window.alert("Quiz Complete!");
+  }
+}
 
 var selectAnswer = function (){
     var q = questions[currentQuesitonIndex];
     if(this.value === q.correctAnswer){
-      alert("correct")
+      console.log("Correct") 
     }
     else{
-      alert("wrong");
+      console.log("Wrong");
       seconds -10;
     }
-    
-};
 
+    nextQuestion();
+    }
 
+var nextQuestion = function() {
+      window.confirm("Next Question?");
+      currentQuesitonIndex ++;
+      renderQuestion()
+  }
 //     currentQuestionIndex ++
 //     nextQuestion();
 // }
@@ -123,3 +126,4 @@ var selectAnswer = function (){
 //once no more question OR no more time = "all done" show score or time and create field to enter initials button to submit
 
 startButton.addEventListener("click", startQuiz);
+aAnswerEl.addEventListener("click", selectAnswer);
